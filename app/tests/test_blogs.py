126288cf7d5f777c4268e06a83f38030b2ad70c8
PaulienLem <paulien.lemay@gmail.com>
2026-02-25 13:47:47 +0100

    Remove comments

--- app/tests/test_blogs.py
+++ app/tests/test_blogs.py
@@ -6,25 +6,14 @@ def run_test():
     sqlite_conn, sqlite_cursor = get_sqlite_connection()
     pg_conn, pg_cursor = get_postgres_connection()
 
-    # ----------------------------
-    # SQLITE SIDE
-    # ----------------------------
     sqlite_cursor.execute("SELECT id, url FROM blog")
     sqlite_set = {(normalize_id(row[0]), normalize_string(row[1])) for row in sqlite_cursor.fetchall()}
 
-    # ----------------------------
-    # POSTGRES SIDE
-    # ----------------------------
     pg_cursor.execute('SELECT identity, url FROM data.blog')
     pg_set = {(normalize_id(row[0]), normalize_string(row[1])) for row in pg_cursor.fetchall()}
 
-    # ----------------------------
-    # Close connections
-    # ----------------------------
+
     sqlite_conn.close()
     pg_conn.close()
 
-    # ----------------------------
-    # Compare
-    # ----------------------------
     return compare_sets("blogs", sqlite_set, pg_set, label="Blog ID / URL")
